febdd9ce612467aae7517e304aac814bbf197136
YuanChengLang <709364178@qq.com>
2020-11-28 14:04:12 +0800

    fix

--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -25,17 +25,17 @@ export default {
                     (<li class={"item"}>
                         <p class={'file-name'}>{file.name}</p>
                         {
-                            file.isUpload 
-                            ? <progress class={'upload-progress'} min="0" max="100" value={file.onPercent}/>
+                            file.uploading 
+                            ? <progress class={'upload-progress'} min="0" max="100" value={file.progress}/>
                             : ''
                         }
                         {
-                            file.isUpload 
-                            ? (file.onPercent > 0 && file.onPercent <= 100)  
-                                ? <span class="percent">已上传 {file.onPercent.toFixed(0)}%</span>
+                            file.uploading 
+                            ? (file.progress > 0 && file.progress <= 100)  
+                                ? <span class="percent">已上传 {file.progress.toFixed(0)}%</span>
                                 : <span class='waiting'>waiting...</span>
-                            : file.onPercent !== 0 
-                                ? file.onPercent >= 100
+                            : file.progress !== 0 
+                                ? file.progress >= 100
                                     ?<span class={'success'}>done!</span> 
                                     : <span class={'fail'}>fail!</span>
                                 : ''
